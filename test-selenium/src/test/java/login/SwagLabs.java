8ae8299ac08376b958d376fb28c97047973d41ed
huy20222003 <huy20222003@gmail.com>
2024-10-06 22:06:00 +0700

    initial project

---
+++ test-selenium/src/test/java/login/SwagLabs.java
@@ -0,0 +1,41 @@
+package login;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SwagLabs {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.manage().window().maximize();
+
+            driver.get("https://www.saucedemo.com/");
+
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+            driver.findElement(By.id("login-button")).click();
+
+            Thread.sleep(2000);
+
+            String currentUrl = driver.getCurrentUrl();
+            if (currentUrl.equals("https://www.saucedemo.com/inventory.html")) {
+                System.out.println("Login successful. Redirected to main page.");
+            } else {
+                System.out.println("Login failed. Current URL: " + currentUrl);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+}
